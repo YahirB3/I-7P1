99b3cb50503c9c40cb40b3465d1a1a1e0ab1b277
YahirB3 <119085450+YahirB3@users.noreply.github.com>
2025-09-18 23:11:46 -0600

    Delete ejercicio_1.1.py

--- ejercicio_1.1.py
+++
@@ -1,8 +0,0 @@
-A = 7
-B = 6
-if A < B:
-    print("El programa A es mas rapido.")
-    print("Ahorro:", B - A, "segundos")
-else:
-    print("El programa B es mas rapido.")
-    print("Ahorro:", A - B, "segundos")
